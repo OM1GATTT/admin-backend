ea4a9e56097ac09fe47ea628fa256e50a0c08d8e
om1ga <845085139@qq.com>
2023-04-21 21:11:04 +0800

    完成部分通知模块业务-增删改查

--- admin-biz/src/main/java/top/om1ga/controller/NoticeController.java
+++ admin-biz/src/main/java/top/om1ga/controller/NoticeController.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import top.om1ga.common.utils.PageResult;
 import top.om1ga.common.utils.Result;
+import top.om1ga.convert.NoticeConvert;
+import top.om1ga.entity.NoticeEntity;
 import top.om1ga.query.NoticeQuery;
 import top.om1ga.service.NoticeService;
 import top.om1ga.vo.NoticeVO;
@@ -27,12 +29,13 @@ import java.util.List;
 @RequestMapping("notice")
 @Tag(name="通知管理")
 @AllArgsConstructor
+@Slf4j
 public class NoticeController {
     private final NoticeService noticeService;
 
     @GetMapping("page")
     @Operation(summary = "通知分页")
-//    @PreAuthorize("hasAuthority('sys:notice:page')")
+    @PreAuthorize("hasAuthority('sys:notice:page')")
     public Result<PageResult<NoticeVO>> page (@ParameterObject @Valid NoticeQuery query){
         PageResult<NoticeVO> page = noticeService.page(query);
         return Result.ok(page);
@@ -43,5 +46,39 @@ public class NoticeController {
     public Result<List<NoticeVO>> list(){
         List<NoticeVO> list = noticeService.getList();
         return Result.ok(list);
+    }
+
+    @GetMapping("{id}")
+    @Operation(summary = "通知信息")
+    @PreAuthorize("hasAuthority('sys:notice:info')")
+    public Result<NoticeVO> get(@PathVariable("id")Long id){
+        NoticeEntity entity = noticeService.getById(id);
+        return Result.ok(NoticeConvert.INSTANCE.convert(entity));
+    }
+
+    @PostMapping
+    @Operation(summary = "新增通知")
+    @PreAuthorize("hasAuthority('sys:notice:save')")
+    public Result<String> save (@RequestBody NoticeVO vo){
+        noticeService.save(vo);
+        return Result.ok();
+    }
+
+
+    @PutMapping
+    @Operation(summary = "修改通知")
+    @PreAuthorize("hasAuthority('sys:notice:update')")
+    public Result<String> update (@RequestBody @Valid NoticeVO vo){
+        log.info(vo.toString());
+        noticeService.update(vo);
+        return Result.ok();
+    }
+
+    @DeleteMapping("{id}")
+    @Operation(summary = "删除通知")
+    @PreAuthorize("hasAuthority('sys:notice:delete')")
+    public Result<String> delete (@PathVariable("id")Long id){
+        noticeService.delete(id);
+        return Result.ok();
     }
 }
